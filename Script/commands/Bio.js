f73270f9e7b05fdae036f576308ee06886477af7
shawonvau00 <sayanmaxtan@gmail.com>
2025-08-03 10:16:07 +0300

    Update Bio.js

--- Script/commands/Bio.js
+++ Script/commands/Bio.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "bio",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "PINIK-SHAWON☢️_𖣘 -𝐁𝐎𝐓 ⚠️",
 	description: "Change bot's bio",
 	commandCategory: "admin",
 	usages: "bio [text]",
